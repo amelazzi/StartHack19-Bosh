6784b84265f119ab535c489a944004beb526286a
Amel AZZI <ea_azzi@esi.dz>
2019-03-09 22:42:07 +0100

    add: basic educational mode interface

---
+++ app/src/main/java/com/example/macos/starthack19_bosh/EducationalActivity.java
@@ -0,0 +1,13 @@
+package com.example.macos.starthack19_bosh;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EducationalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_educational);
+    }
+}
--- app/src/main/java/com/example/macos/starthack19_bosh/MainActivity.java
+++ app/src/main/java/com/example/macos/starthack19_bosh/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.macos.starthack19_bosh;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button educationalMode, normalMode;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        educationalMode = findViewById(R.id.educationalMode);
+        educationalMode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, EducationalActivity.class));
+            }
+        });
+
+        normalMode = findViewById(R.id.normalMode);
+        normalMode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, NormalActivity.class));
+            }
+        });
     }
 }
